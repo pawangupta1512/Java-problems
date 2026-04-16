5b0654b7f1665dc79f095ddcbd04a599cc292ca4
Pawan Gupta <pg948162@gmail.com>
2026-04-16 15:19:52 +0530

    Added arrays, methods, strings and patterns README

---
+++ src/strings/palindrome.java
@@ -0,0 +1,24 @@
+package strings;
+
+import java.util.Scanner;
+
+    public class palindrome {
+    
+
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String s = sc.nextLine();
+        String reverse = "";
+        for (int i = s.length() - 1; i>=0; i--) {
+            reverse = reverse + s.charAt(i);
+        }
+        if (s.equals(reverse)) {
+            System.out.println(s + " is a Palindrome.");
+        } else {
+            System.out.println(s + " is Not Palindrome.");
+        }
+         sc.close();
+    }
+}
+ 
